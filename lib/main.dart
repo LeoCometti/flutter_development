0acf106ccbdf5e538a0855a3aaebf367ceafb862
leonam <cometti@gmail.com>
2021-03-07 18:34:49 -0300

    Adding interactivity

--- lib/main.dart
+++ lib/main.dart
@@ -33,11 +33,12 @@ class MyApp extends StatelessWidget {
             ),
           ),
           /*3*/
-          Icon(
+          FavoriteWidget(),
+          /*Icon(
             Icons.star,
             color: Colors.red[500],
           ),
-          Text('41'),
+          Text('41'),*/
         ],
       ),
     );
@@ -127,3 +128,49 @@ class MyApp extends StatelessWidget {
       padded along each edge by 32 pixels. Add the title section to
       the app body like this:
 */
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoritedCount = 41;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            padding: EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoritedCount'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoritedCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoritedCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+}
